8ee19bf883dc041546d74e5e2d2655331b4fc98c
ldeeley <lester.deeley@yahoo.com>
2023-03-12 17:57:48 +0000

    small changes

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("/")
     public String home(){
+        logger.trace("trace level home method accessed ");
+        logger.info("this is info logging from Album Controller");
         return "Hello World";
     }
 
